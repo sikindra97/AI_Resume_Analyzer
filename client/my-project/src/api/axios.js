49523a58d06204456aa0f235a5891dbd84f969d5
sikindra97 <sikindramahto374@gmail.com>
2026-09-02 14:19:09 +0530

    Update axios.js

--- client/my-project/src/api/axios.js
+++ client/my-project/src/api/axios.js
@@ -1,18 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:https:"ai-resume-analyzer-api-b89v.onrender.com/api",
+  baseURL: "https://ai-resume-analyzer-api-b89v.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
-
-  const token =
-    localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
   if (token) {
-
-    config.headers.Authorization =
-      `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
