4aa2dcb14219e85438c9cc3939143335b1cc0742
sikindra97 <sikindramahto374@gmail.com>
2026-09-02 14:13:17 +0530

    Update axios.js

--- client/my-project/src/api/axios.js
+++ client/my-project/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
-  // baseURL:"https://ai-resume-analyzer-1-ebal.onrender.com/api"
+  baseURL:https:"ai-resume-analyzer-api-b89v.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
